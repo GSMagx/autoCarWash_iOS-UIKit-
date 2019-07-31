d456c5acb796bfced7805e4778c74ceb803955e1
GSMag <gsmagx@me.com>
2019-07-31 10:33:50 +0200

    beta 0.1 Interface

---
+++ CarWash/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarWash
+//
+//  Created by Alex on 29.07.19.
+//  Copyright © 2019 Alex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
